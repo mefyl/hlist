4b81dacd27d42befa7d5757e45878da5c61bb715
mefyl <mefyl@gruntech.org>
2023-01-22 22:09:43 +0100

    Import codebase.

---
+++ lib/src/hlist.ml
@@ -0,0 +1,273 @@
+include Hlist_intf
+
+module Make_ (Base : sig
+  type 't value
+
+  type 't t =
+    | ( :: ) : 'hd value * 'tl t -> ('hd -> 'tl) t
+    | [] : unit t
+end) : S_OPS with type 'a t := 'a Base.t and type 'a value := 'a Base.value =
+struct
+  include Base
+
+  let length l =
+    let rec loop : type l. int -> l t -> int =
+     fun acc -> function _ :: tl -> loop (acc + 1) tl | [] -> acc
+    in
+    loop 0 l
+
+  type 'res imapper = { f : 't. int -> 't value -> 'res }
+
+  let filter_mapi l ~(f : 'res option imapper) =
+    let rec map : type a. int -> a t -> 'res list =
+     fun i -> function
+      | hd :: tl -> (
+        match f.f i hd with
+        | Some value -> List.cons value (map (i + 1) tl)
+        | None -> map (i + 1) tl)
+      | [] -> []
+    in
+    map 0 l
+
+  let mapi l ~f = filter_mapi ~f:{ f = (fun i x -> Some (f.f i x)) } l
+
+  let iteri l ~f =
+    let (_ : unit list) = mapi ~f l in
+    ()
+
+  let find_mapi l ~(f : 'res option imapper) =
+    let rec loop : type a. int -> a t -> 'res option =
+     fun i -> function
+      | hd :: tl -> (
+        match f.f i hd with Some value -> Some value | None -> loop (i + 1) tl)
+      | [] -> None
+    in
+    loop 0 l
+
+  type 'res mapper = { f : 't. 't value -> 'res }
+
+  let filter_map l ~(f : 'res option mapper) =
+    filter_mapi l ~f:{ f = (fun _ v -> f.f v) }
+
+  let find_map l ~(f : 'res option mapper) =
+    find_mapi l ~f:{ f = (fun _ v -> f.f v) }
+
+  let map l ~f = filter_map ~f:{ f = (fun x -> Some (f.f x)) } l
+  let iter l ~f = map ~f l |> ignore
+
+  module Transduce (To : S_BASE) = struct
+    type imapper = { f : 't. int -> 't value -> 't To.value }
+
+    let mapi l ~f:{ f } =
+      let rec map : type a. int -> a t -> a To.t =
+       fun i -> function hd :: tl -> f i hd :: map (i + 1) tl | [] -> []
+      in
+      map 0 l
+
+    type mapper = { f : 't. 't value -> 't To.value }
+
+    let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+  end
+
+  module Transduce2 (To : S2_BASE) = struct
+    type 't folding_mapper = {
+      f : 'a 'b. 't -> 'a value -> 't * ('a, 'b) To.value;
+    }
+
+    let folding_map folded l ~f:{ f } =
+      let rec map : type a b. 'fold -> a t -> (a, b) To.t =
+       fun folded -> function
+        | hd :: tl ->
+          let folded, hd = f folded hd in
+          hd :: map folded tl
+        | [] -> []
+      in
+      map folded l
+
+    type imapper = { f : 'a 'b. int -> 'a value -> ('a, 'b) To.value }
+
+    let mapi l ~f:{ f } =
+      folding_map 0 l ~f:{ f = (fun i elt -> (i + 1, f i elt)) }
+
+    type mapper = { f : 'a 'b. 'a value -> ('a, 'b) To.value }
+
+    let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+  end
+end
+
+include Make_ (struct
+  type 'a value = 'a
+
+  type 'a t = 'a Select_schema.Hlist.t =
+    | ( :: ) : 'hd value * 'tl t -> ('hd -> 'tl) t
+    | [] : unit t
+end)
+
+module Make (T : sig
+  type 't t
+end) =
+struct
+  type 't value = 't T.t
+
+  type 't list =
+    | ( :: ) : 'hd value * 'tl list -> ('hd -> 'tl) list
+    | [] : unit list
+
+  include Make_ (struct
+    type nonrec 't value = 't value
+
+    type 't t = 't list =
+      | ( :: ) : 'hd value * 'tl t -> ('hd -> 'tl) t
+      | [] : unit t
+  end)
+
+  type 't t = 't list =
+    | ( :: ) : 'hd value * 'tl list -> ('hd -> 'tl) t
+    | [] : unit t
+end
+
+include Select_schema.Hlist
+
+type (_, _) convert =
+  | Cons : ('b, 'c) convert -> ('a -> 'b, 'a * 'c) convert
+  | Null : (unit, unit) convert
+
+let rec to_pairs : type a b. (a, b) convert -> a t -> b =
+ fun convert l ->
+  match (convert, l) with
+  | Cons next, a :: b -> (a, to_pairs next b)
+  | Null, [] -> ()
+
+module Make2 (T : sig
+  type ('a, 'b) t
+end) : S2 with type ('a, 'b) value = ('a, 'b) T.t = struct
+  type ('t, 'tag) value = ('t, 'tag) T.t
+
+  type ('t, 'tag) t =
+    | ( :: ) : ('hd, 'tag) value * ('tl, 'tag) t -> ('hd -> 'tl, 'tag) t
+    | [] : (unit, 'tag) t
+
+  let length l =
+    let rec loop : type l. int -> (l, 'tag) t -> int =
+     fun acc -> function _ :: tl -> loop (acc + 1) tl | [] -> acc
+    in
+    loop 0 l
+
+  type ('b, 'res) imapper = { f : 'a. int -> ('a, 'b) value -> 'res }
+
+  let filter_mapi l ~f =
+    let rec map : type a. int -> (a, 'tag) t -> 'res list =
+     fun i -> function
+      | hd :: tl -> (
+        match f.f i hd with
+        | Some value -> List.cons value (map (i + 1) tl)
+        | None -> map (i + 1) tl)
+      | [] -> []
+    in
+    map 0 l
+
+  let mapi l ~f = filter_mapi ~f:{ f = (fun i x -> Some (f.f i x)) } l
+
+  let iteri l ~f =
+    let (_ : unit list) = mapi ~f l in
+    ()
+
+  let find_mapi l ~(f : ('b, 'res option) imapper) =
+    let rec loop : type a. int -> (a, 'b) t -> 'res option =
+     fun i -> function
+      | hd :: tl -> (
+        match f.f i hd with Some value -> Some value | None -> loop (i + 1) tl)
+      | [] -> None
+    in
+    loop 0 l
+
+  type ('b, 'res) mapper = { f : 'a. ('a, 'b) value -> 'res }
+
+  let filter_map l ~f = filter_mapi l ~f:{ f = (fun _ v -> f.f v) }
+
+  let find_map l ~(f : ('b, 'res option) mapper) =
+    find_mapi l ~f:{ f = (fun _ v -> f.f v) }
+
+  let map l ~f = filter_map ~f:{ f = (fun x -> Some (f.f x)) } l
+  let iter l ~f = map ~f l |> ignore
+
+  module Transduce (To : S_BASE) = struct
+    type imapper = { f : 'a 'b. int -> ('a, 'b) value -> 'a To.value }
+
+    let mapi l ~f:{ f } =
+      let rec map : type a b. int -> (a, b) t -> a To.t =
+       fun i -> function hd :: tl -> f i hd :: map (i + 1) tl | [] -> []
+      in
+      map 0 l
+
+    type mapper = { f : 'a 'b. ('a, 'b) value -> 'a To.value }
+
+    let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+  end
+
+  module Transduce2 (To : S2_BASE) = struct
+    type imapper = { f : 'a 'b 'c. int -> ('a, 'b) value -> ('a, 'c) To.value }
+
+    let mapi l ~f:{ f } =
+      let rec map : type a b c. int -> (a, b) t -> (a, c) To.t =
+       fun i -> function hd :: tl -> f i hd :: map (i + 1) tl | [] -> []
+      in
+      map 0 l
+
+    type mapper = { f : 'a 'b 'c. ('a, 'b) value -> ('a, 'c) To.value }
+
+    let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+  end
+end
+
+module Zip2 (L : S2) (R : S2) = struct
+  include Make2 (struct
+    type ('a, 'b) t = ('a, 'b) L.value * ('a, 'b) R.value
+  end)
+
+  let zip l r =
+    let rec zip : type a. (a, 'b) L.t * (a, 'b) R.t -> (a, 'b) t = function
+      | L.(hd_l :: tl_l), R.(hd_r :: tl_r) -> (hd_l, hd_r) :: zip (tl_l, tl_r)
+      | L.[], R.[] -> []
+    in
+    zip (l, r)
+end
+
+(* module Transduce (To : S_BASE) = struct
+ *   type imapper = { f : 't. int -> 't value -> 't To.value }
+ *
+ *   let mapi l ~f:{ f } =
+ *     let rec map : type a. int -> a t -> a To.t =
+ *      fun i -> function hd :: tl -> f i hd :: map (i + 1) tl | [] -> []
+ *     in
+ *     map 0 l
+ *
+ *   type mapper = { f : 't. 't value -> 't To.value }
+ *
+ *   let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+ * end
+ *
+ * module Transduce2 (To : S2_BASE) = struct
+ *   type 't folding_mapper = {
+ *     f : 'a 'b. 't -> 'a value -> 't * ('a, 'b) To.value;
+ *   }
+ *
+ *   let folding_map folded l ~f:{ f } =
+ *     let rec map : type a b. 'fold -> a t -> (a, b) To.t =
+ *      fun folded -> function
+ *       | hd :: tl ->
+ *         let folded, hd = f folded hd in
+ *         hd :: map folded tl
+ *       | [] -> []
+ *     in
+ *     map folded l
+ *
+ *   type imapper = { f : 'a 'b. int -> 'a value -> ('a, 'b) To.value }
+ *
+ *   let mapi l ~f:{ f } =
+ *     folding_map 0 l ~f:{ f = (fun i elt -> (i + 1, f i elt)) }
+ *
+ *   type mapper = { f : 'a 'b. 'a value -> ('a, 'b) To.value }
+ *
+ *   let map l ~f:{ f } = mapi l ~f:{ f = (fun _ v -> f v) }
+ * end *)
---
+++ lib/src/hlist_intf.ml
@@ -0,0 +1,105 @@
+module type S_BASE = sig
+  type 'a value
+
+  type 'a t =
+    | ( :: ) : 'hd value * 'tl t -> ('hd -> 'tl) t
+    | [] : unit t
+end
+
+module type S2_BASE = sig
+  type ('a, 'b) value
+
+  type ('a, 'b) t =
+    | ( :: ) : ('hd, 'b) value * ('tl, 'b) t -> ('hd -> 'tl, 'b) t
+    | [] : (unit, 'b) t
+end
+
+module type S_OPS = sig
+  type 'a t
+  type 'a value
+
+  val length : _ t -> int
+
+  type 'res mapper = { f : 'a. 'a value -> 'res }
+  type 'res imapper = { f : 'a. int -> 'a value -> 'res }
+
+  val filter_map : 'a t -> f:'res option mapper -> 'res list
+  val filter_mapi : 'a t -> f:'res option imapper -> 'res list
+  val find_map : 'a t -> f:'res option mapper -> 'res option
+  val find_mapi : 'a t -> f:'res option imapper -> 'res option
+  val iter : 'a t -> f:unit mapper -> unit
+  val iteri : 'a t -> f:unit imapper -> unit
+  val map : 'a t -> f:'res mapper -> 'res list
+  val mapi : 'a t -> f:'res imapper -> 'res list
+
+  module Transduce (To : S_BASE) : sig
+    type mapper = { f : 'a. 'a value -> 'a To.value }
+
+    val map : 'a t -> f:mapper -> 'a To.t
+
+    type imapper = { f : 'a. int -> 'a value -> 'a To.value }
+
+    val mapi : 'a t -> f:imapper -> 'a To.t
+  end
+
+  module Transduce2 (To : S2_BASE) : sig
+    type mapper = { f : 'a 'b. 'a value -> ('a, 'b) To.value }
+
+    val map : 'a t -> f:mapper -> ('a, 'b) To.t
+
+    type imapper = { f : 'a 'b. int -> 'a value -> ('a, 'b) To.value }
+
+    val mapi : 'a t -> f:imapper -> ('a, 'b) To.t
+
+    type 't folding_mapper = {
+      f : 'a 'b. 't -> 'a value -> 't * ('a, 'b) To.value;
+    }
+
+    val folding_map : 'fold -> 'a t -> f:'fold folding_mapper -> ('a, 'b) To.t
+  end
+end
+
+module type S = sig
+  include S_BASE
+  include S_OPS with type 'a t := 'a t and type 'a value := 'a value
+end
+
+module type S2 = sig
+  include S2_BASE
+
+  val length : _ t -> int
+
+  type ('b, 'res) mapper = { f : 'a. ('a, 'b) value -> 'res }
+  type ('b, 'res) imapper = { f : 'a. int -> ('a, 'b) value -> 'res }
+
+  val filter_map : ('a, 'b) t -> f:('b, 'res option) mapper -> 'res list
+  val filter_mapi : ('a, 'b) t -> f:('b, 'res option) imapper -> 'res list
+  val find_map : ('a, 'b) t -> f:('b, 'res option) mapper -> 'res option
+  val find_mapi : ('a, 'b) t -> f:('b, 'res option) imapper -> 'res option
+  val iter : ('a, 'b) t -> f:('b, unit) mapper -> unit
+  val iteri : ('a, 'b) t -> f:('b, unit) imapper -> unit
+  val map : ('a, 'b) t -> f:('b, 'res) mapper -> 'res list
+  val mapi : ('a, 'b) t -> f:('b, 'res) imapper -> 'res list
+
+  module Transduce (To : S_BASE) : sig
+    type mapper = { f : 'a 'b. ('a, 'b) value -> 'a To.value }
+
+    val map : ('a, 'b) t -> f:mapper -> 'a To.t
+
+    type imapper = { f : 'a 'b. int -> ('a, 'b) value -> 'a To.value }
+
+    val mapi : ('a, 'b) t -> f:imapper -> 'a To.t
+  end
+
+  module Transduce2 (To : S2_BASE) : sig
+    type mapper = { f : 'a 'b1 'b2. ('a, 'b1) value -> ('a, 'b2) To.value }
+
+    val map : ('a, 'b1) t -> f:mapper -> ('a, 'b2) To.t
+
+    type imapper = {
+      f : 'a 'b1 'b2. int -> ('a, 'b1) value -> ('a, 'b2) To.value;
+    }
+
+    val mapi : ('a, 'b1) t -> f:imapper -> ('a, 'b2) To.t
+  end
+end
